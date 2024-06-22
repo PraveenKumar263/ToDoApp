4e9fa387acdec142bc8ced04068fa712241c7c91
PraveenKumar263 <PraveenKumar263>
2024-06-22 15:51:12 +0800

    added updateTask function and added TasksSection component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import './App.css';
 import { Header } from './components/Header';
 import { Form } from './components/Form';
 import { useState } from 'react';
+import INITIAL_TASKS from './data/taskData.json';
+import { TasksSection } from './components/TasksSection';
+import { useEffect } from "react";
 
 function App() {
-  const [taskList, setTaskList] = useState([]);
+
+  // Manage tasks
+  const [taskList, setTaskList] = useState(INITIAL_TASKS);
+  
+  // To add a new task
   const addTask = newTask => setTaskList([...taskList, newTask]);
-  const removeTask = taskToRemove  => setTaskList(taskList.filter(task => task !== taskToRemove ));
+  
+  // To remove task
+  const removeTask = taskToRemove  => setTaskList(taskList.filter(task => 
+    task !== taskToRemove ));
+
+  // To update existing task
+  const updateTask = (updatedTask) => {
+    const updatedList = taskList.map(task =>
+        task.id === updatedTask.id ? updatedTask : task
+    );
+    setTaskList(updatedList);
+  };
+  useEffect(() => {
+    // console.log("Task List Updated:", taskList);
+}, [taskList]); 
   return (
     <div className="App">
       <Header />
       <Form taskList={taskList} addTask={addTask} />
-
+      <TasksSection taskList={taskList} removeTask={removeTask} updateTask={updateTask}/>
     </div>
   );
 }
